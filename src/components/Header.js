cd4ebd97c2415d0f546588e1159d429b960428b5
ssword <ssword@gmail.com>
2021-01-16 06:21:53 +0800

    fix on file name

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header className="h-16 bg-blue-600 text-white sticky top-0 z-50">
+    <div className="flex justify-between items-center px-8 mx-auto w-full h-full max-w-screen-xl">
+      <h1 className="text-2xl font-semibold font-rubik">
+        <Link to="/"> {siteTitle} </Link>
+      </h1>
+      <ul className="text-lg font-thin">
+        <li>
+          <Link to="/">Blog</Link>
+        </li>
+      </ul>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
